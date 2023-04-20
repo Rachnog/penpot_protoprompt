c79fcf942760341ee65650ce272c7ae3cb6e16c2
Alex Honchar <rachnogstyle@gmail.com>
2023-04-20 08:45:43 +0200

    minor quality check fixes

--- src/svg_quality_checks.py
+++ src/svg_quality_checks.py
@@ -10,13 +10,20 @@ def validate_svg(svg_string):
     return True
 
 def get_svg_size(svg_string):
-    # Parse the SVG string as an ElementTree object
-    svg_root = ET.fromstring(svg_string)
-    
-    # Get the 'width' and 'height' attributes from the root element
-    width = svg_root.get('width')
-    height = svg_root.get('height')
-    
+    try:
+        # Parse the SVG string as an ElementTree object
+        svg_root = ET.fromstring(svg_string)
+        # Get the 'width' and 'height' attributes from the root element
+        width = svg_root.get('width')
+        height = svg_root.get('height')
+    except:
+        width = -1
+        height = -1
+    finally:
+        if width is None:
+            width = -1
+        if height is None:
+            height = -1
     # Convert the strings to floats and return as a tuple
     return (float(width), float(height))
 
